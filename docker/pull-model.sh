33e772e84745b3def02667da413610f2201cb5b7
Seokjae Lee <seok9211@naver.com>
2024-10-15 16:15:34 +0900

    feat: ollama configuration

---
+++ docker/pull-model.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/78500319/how-to-pull-model-automatically-with-container-creation
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve all-minilm model..."
+ollama pull all-minilm
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
