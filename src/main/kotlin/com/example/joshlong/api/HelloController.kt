d017638fc1cf5e9af96dc975fef3471a442b9e6d
Seokjae Lee <seok9211@naver.com>
2024-10-15 19:17:29 +0900

    feat: embedding search

--- src/main/kotlin/com/example/joshlong/api/HelloController.kt
+++ src/main/kotlin/com/example/joshlong/api/HelloController.kt
@@ -2,6 +2,8 @@ package com.example.joshlong.api
 
 import com.example.joshlong.service.HelloService
 import org.springframework.ai.document.Document
+import org.springframework.ai.embedding.EmbeddingModel
+import org.springframework.ai.embedding.EmbeddingResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,10 +11,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController(
     private val helloService: HelloService,
+    private val embeddingModel: EmbeddingModel,
 ) {
 
     @GetMapping("/hello")
     fun hello(@RequestParam query: String): MutableList<Document>? {
         return helloService.hello(query)
+    }
+
+    // https://docs.spring.io/spring-ai/reference/api/embeddings/ollama-embeddings.html
+    @GetMapping("/ai/embedding")
+    fun embed(
+        @RequestParam(
+            defaultValue = "Tell me a joke"
+        ) message: String,
+    ): Map<String, EmbeddingResponse> {
+        val embeddingResponse: EmbeddingResponse = embeddingModel.embedForResponse(listOf(message))
+
+        return mapOf("embedding" to embeddingResponse)
     }
 }
